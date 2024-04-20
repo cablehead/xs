fca19ed6d1f93da1064b4724cbfb32a2505f491d
Andy Gayton <andy@thecablelounge.com>
2024-04-19 22:28:35 -0400

    wip

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,13 @@
+#![allow(warnings)]
+use std::error::Error;
+
 use tokio::net::UnixListener;
 
 use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{StatusCode};
+use hyper::StatusCode;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 
@@ -16,10 +19,11 @@ async fn handle(req: Request<hyper::body::Incoming>) -> HTTPResult {
     Ok(Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/html")
-        .body(full(preview)))
+        .body(full(preview))?)
 }
 
 pub async fn serve() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("let's go");
     let listener = UnixListener::bind("./sock").unwrap();
     loop {
         let (stream, _) = listener.accept().await?;
@@ -29,20 +33,36 @@ pub async fn serve() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .serve_connection(io, service_fn(move |req| handle(req)))
                 .await
             {
-                eprintln!("Error serving connection: {:?}", err);
+                // Match against the error kind to selectively ignore `NotConnected` errors
+                if let Some(std::io::ErrorKind::NotConnected) = err.source().and_then(|source| {
+                    source
+                        .downcast_ref::<std::io::Error>()
+                        .map(|io_err| io_err.kind())
+                }) {
+                    // Silently ignore the NotConnected error
+                } else {
+                    // Handle or log other errors
+                    println!("Error serving connection: {:?}", err);
+                }
             }
         });
     }
 }
 
-// fit our broadened Response body type.
-fn empty() -> BoxBody<Bytes, hyper::Error> {
-    Empty::<Bytes>::new()
+fn response_404() -> HTTPResult {
+    Ok(Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(empty())?)
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, BoxError> {
+    Full::new(chunk.into())
         .map_err(|never| match never {})
         .boxed()
 }
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-    Full::new(chunk.into())
+
+fn empty() -> BoxBody<Bytes, BoxError> {
+    Empty::<Bytes>::new()
         .map_err(|never| match never {})
         .boxed()
 }
