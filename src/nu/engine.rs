19c8a319d4babfe4df0dab5a0e3f62edc406edbe
Andy Gayton <andy@thecablelounge.com>
2024-08-14 15:58:01 -0400

    rework src/nu's api: wip

--- src/nu/engine.rs
+++ src/nu/engine.rs
@@ -2,39 +2,83 @@ use super::commands::add_custom_commands;
 use super::util;
 use crate::error::Error;
 use crate::store::{Frame, Store};
+use nu_cli::{add_cli_context, gather_parent_env_vars};
 use nu_cmd_lang::create_default_context;
+use nu_command::add_shell_command_context;
+use nu_engine::eval_block;
+use nu_parser::parse;
 use nu_protocol::debugger::WithoutDebug;
-use nu_protocol::engine::{Closure, EngineState, Stack};
+use nu_protocol::engine::{Closure, EngineState, Stack, StateWorkingSet};
 use nu_protocol::{PipelineData, ShellError, Span, Value};
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+pub struct ThreadPool {
+    workers: Vec<tokio::task::JoinHandle<()>>,
+    sender: crossbeam_channel::Sender<Box<dyn FnOnce() + Send + 'static>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        let (sender, receiver) = crossbeam_channel::unbounded();
+        let receiver = Arc::new(receiver);
+
+        let workers = (0..size)
+            .map(|_| {
+                let receiver = receiver.clone();
+                tokio::spawn(async move {
+                    while let Ok(job) = receiver.recv() {
+                        job();
+                    }
+                })
+            })
+            .collect();
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        self.sender.send(Box::new(f)).unwrap();
+    }
+}
 
 #[derive(Clone)]
 pub struct Engine {
     engine_state: EngineState,
     store: Store,
+    pool: Arc<ThreadPool>,
     active_count: Arc<AtomicUsize>,
 }
 
 impl Engine {
-    pub fn new(store: Store) -> Self {
+    pub fn new(store: Store, thread_count: usize) -> Result<Self, Error> {
         let mut engine_state = create_default_context();
+        engine_state = add_shell_command_context(engine_state);
+        engine_state = add_cli_context(engine_state);
         engine_state = add_custom_commands(store.clone(), engine_state);
-        Self {
+
+        let init_cwd = std::env::current_dir()?;
+        gather_parent_env_vars(&mut engine_state, init_cwd.as_ref());
+
+        Ok(Self {
             engine_state,
             store,
+            pool: Arc::new(ThreadPool::new(thread_count)),
             active_count: Arc::new(AtomicUsize::new(0)),
-        }
+        })
     }
 
     pub fn parse_closure(&self, closure_snippet: &str) -> Result<Closure, ShellError> {
-        let mut working_set = nu_protocol::engine::StateWorkingSet::new(&self.engine_state);
-        let block = nu_parser::parse(&mut working_set, None, closure_snippet.as_bytes(), false);
+        let mut working_set = StateWorkingSet::new(&self.engine_state);
+        let block = parse(&mut working_set, None, closure_snippet.as_bytes(), false);
         let mut engine_state = self.engine_state.clone();
         engine_state.merge_delta(working_set.render())?;
 
         let mut stack = Stack::new();
-        let result = nu_engine::eval_block::<WithoutDebug>(
+        let result = eval_block::<WithoutDebug>(
             &engine_state,
             &mut stack,
             &block,
@@ -47,20 +91,22 @@ impl Engine {
         self.active_count.fetch_add(1, Ordering::SeqCst);
         let engine_state = self.engine_state.clone();
         let closure = closure.clone();
+        let pool = self.pool.clone();
         let active_count = self.active_count.clone();
 
-        tokio::task::spawn_blocking(move || {
+        let (tx, rx) = tokio::sync::oneshot::channel();
+
+        pool.execute(move || {
             let input = PipelineData::Value(util::frame_to_value(&frame, Span::unknown()), None);
             let result = match eval_closure(&engine_state, &closure, input) {
                 Ok(pipeline_data) => pipeline_data.into_value(Span::unknown()),
                 Err(err) => Err(err),
             };
             active_count.fetch_sub(1, Ordering::SeqCst);
-            result
-        })
-        .await
-        .unwrap()
-        .map_err(Error::from)
+            let _ = tx.send(result);
+        });
+
+        rx.await.unwrap().map_err(Error::from)
     }
 
     pub async fn wait_for_completion(&self) {
@@ -77,5 +123,5 @@ fn eval_closure(
 ) -> Result<PipelineData, ShellError> {
     let block = engine_state.get_block(closure.block_id);
     let mut stack = Stack::new();
-    nu_engine::eval_block::<WithoutDebug>(engine_state, &mut stack, block, input)
+    eval_block::<WithoutDebug>(engine_state, &mut stack, block, input)
 }
