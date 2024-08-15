73a0c1f60808fb321628f7f4c24db4fdd1102f03
Andy Gayton <andy@thecablelounge.com>
2024-08-14 22:38:57 -0400

    fix(api): accepting the topic had broken, when appending new items

--- tests/integration.rs
+++ tests/integration.rs
@@ -16,17 +16,31 @@ async fn test_integration() {
         .spawn()
         .expect("Failed to start CLI binary");
 
-    // Give the CLI some time to initialize
+    // wait for the listen socket to be created
+    let sock_path = temp_dir.path().join("sock");
+    let start = std::time::Instant::now();
+    loop {
+        if sock_path.exists() {
+            break;
+        }
+
+        if start.elapsed() > Duration::from_secs(2) {
+            panic!("Timeout waiting for sock file to be created");
+        }
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+    }
+
+    // Give the server a moment to start up
     tokio::time::sleep(Duration::from_millis(500)).await;
 
-    let output = cmd!("sh", "-c", format!(
+    let command = format!(
         "echo 123 | curl -s -X POST -T - --unix-socket {}/sock 'localhost/stream/cross/pasteboard?foo=bar'",
         temp_dir.path().display()
-    ))
-        .read()
-        .unwrap();
-
+    );
+    let output = cmd!("sh", "-c", command).read().unwrap();
     let frame: Frame = serde_json::from_str(&output).expect("Failed to parse JSON into Frame");
+
     let output = cmd!(
         "sh",
         "-c",
