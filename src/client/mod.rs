38d9fe2404b8de35be5b82a7e396eb5c8e3aab83
Andy Gayton <andy@thecablelounge.com>
2024-11-07 13:54:02 -0500

    wip

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -5,5 +5,3 @@ mod request;
 pub use self::commands::{append, cas_get, cat, get, head, pipe, remove};
 pub use self::connect::connect;
 pub use self::request::RequestParts;
-
-type BoxError = Box<dyn std::error::Error + Send + Sync>;
