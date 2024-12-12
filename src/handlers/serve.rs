ef5a234e498141ea6fff25790f04ad92e2b29542
Andy Gayton <andy@thecablelounge.com>
2024-12-12 11:51:03 -0500

    wip

--- src/handlers/serve.rs
+++ src/handlers/serve.rs
@@ -3,6 +3,34 @@ use crate::nu;
 use crate::store::{FollowOption, Frame, ReadOptions, Store};
 use crate::thread_pool::ThreadPool;
 
+async fn start_handler(
+    frame: &Frame,
+    store: &Store,
+    engine: &nu::Engine,
+    pool: &ThreadPool,
+    topic: &str,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    match Handler::from_frame(frame, store, engine.clone()).await {
+        Ok(handler) => {
+            handler.spawn(store.clone(), pool.clone()).await?;
+            Ok(())
+        }
+        Err(err) => {
+            let _ = store
+                .append(
+                    Frame::with_topic(format!("{}.unregistered", topic))
+                        .meta(serde_json::json!({
+                            "handler_id": frame.id.to_string(),
+                            "error": err.to_string(),
+                        }))
+                        .build(),
+                )
+                .await;
+            Ok(())
+        }
+    }
+}
+
 pub async fn serve(
     store: Store,
     engine: nu::Engine,
@@ -18,30 +46,12 @@ pub async fn serve(
                 .map(|prefix| prefix.to_string())
         })
         .build();
-
     let mut recver = store.read(options).await;
-
     while let Some(frame) = recver.recv().await {
+        eprintln!("frame: {:?}", frame);
         if let Some(topic) = frame.topic.strip_suffix(".register") {
-            match Handler::from_frame(&frame, &store, engine.clone()).await {
-                Ok(handler) => {
-                    handler.spawn(store.clone(), pool.clone()).await?;
-                }
-                Err(err) => {
-                    let _ = store
-                        .append(
-                            Frame::with_topic(format!("{}.unregistered", topic))
-                                .meta(serde_json::json!({
-                                    "handler_id": frame.id.to_string(),
-                                    "error": err.to_string(),
-                                }))
-                                .build(),
-                        )
-                        .await;
-                }
-            }
+            start_handler(&frame, &store, &engine, &pool, topic).await?;
         }
     }
-
     Ok(())
 }
