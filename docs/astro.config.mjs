d26018e51f2bd962067929e4176cd52f16158b42
Andy Gayton <andy@thecablelounge.com>
2024-12-31 14:45:49 -0500

    wip: deploy docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,9 +6,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'cross.stream',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/cablehead/xs',
 			},
 			sidebar: [
 				{
