21b09f6729ca5f2f2cf0ec3d1ada6bcf3b2381b7
Andy Gayton <andy@thecablelounge.com>
2025-01-01 23:54:43 -0500

    docs: getting started / installation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,30 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/xs/',
-	integrations: [
-		starlight({
-			title: 'xs',
-			social: {
-				github: 'https://github.com/cablehead/xs',
-				discord: 'https://discord.com/invite/YNbScHBHrh',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  base: "/xs/",
+  integrations: [
+    starlight({
+      title: "xs",
+
+      social: {
+        github: "https://github.com/cablehead/xs",
+        discord: "https://discord.com/invite/YNbScHBHrh",
+      },
+
+      sidebar: [
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+
+      expressiveCode: {
+        themes: ["dracula", "rose-pine-dawn"],
+      },
+    }),
+  ],
 });
