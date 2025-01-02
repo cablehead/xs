3563a1defa7dac08127599136ea9dd06698ae6ad
Andy Gayton <andy@thecablelounge.com>
2025-01-02 10:20:10 -0500

    docs: add an architecture overview

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,13 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+
 import starlight from "@astrojs/starlight";
+
+import { rehypeMermaid } from "@beoe/rehype-mermaid";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/xs/",
+
   integrations: [
+    solid(),
+
     starlight({
       title: "xs",
+
+      customCss: [
+        "./src/css/custom.css",
+      ],
 
       social: {
         github: "https://github.com/cablehead/xs",
@@ -34,4 +45,18 @@ export default defineConfig({
       },
     }),
   ],
+
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeMermaid,
+        {
+          strategy: "file", // alternatively use "data-url"
+          fsPath: "public/beoe", // add this to gitignore
+          webPath: "/xs/beoe",
+          darkScheme: "class",
+        },
+      ],
+    ],
+  },
 });
