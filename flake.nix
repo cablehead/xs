45255f70c73e1703a4a68d1e97c44a9f7acac8e9
schlich <ty.schlich@gmail.com>
2025-10-10 13:11:05 -0500

    ci: reintroduce macos runner

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
