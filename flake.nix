7c45a48d4cdd2674cf9f62058f7bcbb4daf19371
schlich <ty.schlich@gmail.com>
2025-10-10 10:15:53 -0500

    feat: add Nix flake with dev shells for Nushell and bash

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "An event stream store for personal, local-first use, specializing in event sourcing.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        src = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter = path: type:
+            (builtins.match ".*xs\.nu$" path != null) ||
+            (craneLib.filterCargoSources path type);
+        };
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = with pkgs; [
+            openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        cross-stream = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          doCheck = false;
+        });
+      in
+      {
+        checks = {
+          inherit cross-stream;
+        };
+
+        packages = {
+          default = cross-stream;
+          cross-stream = cross-stream;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = cross-stream;
+          name = "xs";
+        };
+
+        devShells = {
+          default = craneLib.devShell {
+            checks = self.checks.${system};
+            packages = with pkgs; [
+              rust-analyzer
+              rustfmt
+              clippy
+              nushell
+            ];
+            shellHook = ''
+              nu -i
+            '';
+          };
+
+          bash = craneLib.devShell {
+            checks = self.checks.${system};
+            packages = with pkgs; [
+              rust-analyzer
+              rustfmt
+              clippy
+            ];
+          };
+        };
+      });
+}
