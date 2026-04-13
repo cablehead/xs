5a4f94157dcb367331bd059b39c972861f9b2ed6
Andy Gayton <andy@thecablelounge.com>
2026-04-13 15:31:04 +0000

    chore: bump rust-version to 1.92.0 (required by nu 0.112.1)

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           overlays = [ (import inputs.rust-overlay) ];
         };
 
-        rustToolchain = pkgs.rust-bin.stable."1.91.0".default;
+        rustToolchain = pkgs.rust-bin.stable."1.92.0".default;
 
         craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustToolchain;
 
